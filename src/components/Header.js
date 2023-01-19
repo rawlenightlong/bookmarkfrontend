b6e7403798af2b460a3b2e1cf2961ab3ad9ed998
Gabriel Royce <geroyceus@gmail.com>
2023-01-19 18:15:23 -0500

    added scss styling to the main page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 function Header(props) {
-  return (
-    <nav className="nav">
-      <Link to="/">
-        <div>Bookmarks</div>
-      </Link>
-    </nav>
-  )
+	return (
+		<nav className="nav">
+			<Link to="/" id="navBox">
+				<div>Bookmarks</div>
+			</Link>
+		</nav>
+	);
 }
 
-export default Header
+export default Header;
