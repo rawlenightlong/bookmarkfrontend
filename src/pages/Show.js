85921a1cdf352af237dc686b1ed487b7c5dedaf0
Gabriel Royce <geroyceus@gmail.com>
2023-01-19 19:57:23 -0500

    finished up scss prototype, adding responsive scss

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,35 +6,45 @@ function Show(props) {
 		window.location.replace(clickedURL);
 	};
 	return (
-		<div className="bookmark">
-			<div>{bookmark.title}</div>
-			<div>
-				<Link onClick={() => handleLink(bookmark.url)}>{bookmark.url}</Link>
+		<div className="bookmarkShowPage">
+			<div className="showTitleArea">
+				<div className="showTitle">{bookmark.title}</div>
+				<div className="showLink">
+					<Link onClick={() => handleLink(bookmark.url)}>{bookmark.url}</Link>
+				</div>
 			</div>
-
-			<h2>Update Bookmark</h2>
-			<Form action={`/update/${bookmark._id}`} method="post">
-				<input
-					defaultValue={bookmark.title}
-					type="input"
-					name="title"
-					placeholder="Bookmark Name"
-				/>
-				<input
-					defaultValue={bookmark.url}
-					type="input"
-					name="url"
-					placeholder="Bookmark URL"
-				/>
-				<input type="submit" value={`Update Bookmark}`} />
-			</Form>
-			<h2>Delete Bookmark</h2>
-			<Form action={`/delete/${bookmark._id}`} method="post">
-				<input type="submit" value={`Delete Bookmark`} />
-			</Form>
+			<div className="showControlArea">
+				<div className="showUpdateArea">
+					<h2>Update Bookmark</h2>
+					<Form action={`/update/${bookmark._id}`} method="post">
+						<input
+							defaultValue={bookmark.title}
+							type="input"
+							name="title"
+							placeholder="Bookmark Name"
+						/>
+						<input
+							defaultValue={bookmark.url}
+							type="input"
+							name="url"
+							placeholder="Bookmark URL"
+						/>
+						<input
+							type="submit"
+							value={`Update Bookmark}`}
+							id="showUpdateSubmitButton"
+						/>
+					</Form>
+				</div>
+				<div className="showDeleteArea">
+					<h2>Delete Bookmark</h2>
+					<Form action={`/delete/${bookmark._id}`} method="post">
+						<input type="submit" value={`Delete Bookmark`} />
+					</Form>
+				</div>
+			</div>
 		</div>
 	);
 }
-
 
 export default Show;
